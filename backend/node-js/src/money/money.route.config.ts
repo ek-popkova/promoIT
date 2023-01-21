a92b25f4f5d8695bd66c87050cb7ff9226264a44
Popkova Ekaterina <111788752+ek-popkova@users.noreply.github.com>
2023-01-21 22:29:44 +0200

    extra routes protected

--- backend/node-js/src/money/money.route.config.ts
+++ backend/node-js/src/money/money.route.config.ts
@@ -2,6 +2,7 @@ import { RouteConfig } from "../common/common.route.config"
 import express, { Application, Request, Response } from "express"
 import MoneyController from "./money.controller"
 import { body, check } from "express-validator"
+import AuthMiddleware from "../authentication/authentication.middleware"
 
 
 export class MoneyRoutes extends RouteConfig {
@@ -9,24 +10,24 @@ export class MoneyRoutes extends RouteConfig {
     super(app, "MoneyRoutes")
   }
   configureRoutes() {
-    this.app.route(`/money`).get([MoneyController.getMoneyByHashtagTwitter])
+    this.app.route(`/money`).get([AuthMiddleware.authenticateAccessToken, AuthMiddleware.checkRoles(["Admin"]), MoneyController.getMoneyByHashtagTwitter])
 
     this.app.get(`/money/:id`, [
       check('id').isInt().withMessage("The 'id' parameter must be an integer"), ],
-      [MoneyController.getMoneyBySocialActivistId])
+      [AuthMiddleware.authenticateAccessToken, AuthMiddleware.checkRoles(["Admin"]), MoneyController.getMoneyBySocialActivistId])
 
     //this.app.route(`/money`).post([MoneyController.addMoneyByHashtagTwitter])
     this.app.post(`/money`, [
       body('social_activist_id').isInt().withMessage("The 'social_activist_id' parameter must be an integer"),
       body('campaign_id').isInt().withMessage("The 'campaign_id' parameter must be an integer"),
       body('money').isInt().withMessage("The 'money' parameter must be an integer"),
-    ],[MoneyController.addMoneyByHashtagTwitter])
+    ],[AuthMiddleware.authenticateAccessToken, AuthMiddleware.checkRoles(["Admin"]), MoneyController.addMoneyByHashtagTwitter])
 
     //this.app.route(`/money`).put([MoneyController.updateMoneyByHashtagTwitter])
     this.app.put(`/money`, [
       body('social_activist_id').isInt().withMessage("The 'social_activist_id' parameter must be an integer"),
       body('campaign_id').isInt().withMessage("The 'campaign_id' parameter must be an integer"),
-    ], [MoneyController.updateMoneyByHashtagTwitter])
+    ], [AuthMiddleware.authenticateAccessToken, AuthMiddleware.checkRoles(["Admin"]), MoneyController.updateMoneyByHashtagTwitter])
 
     return this.app
   }
